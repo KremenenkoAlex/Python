10d9e0794bfb844de1ff82607bfba200523bb44a
kremenenko <kremenenko2008@mail.ru>
2021-01-09 21:45:34 +0300

    lesson5

--- lesson5/exp2.py
+++
@@ -1,9 +0,0 @@
-my_func = open("text2.txt", "r")
-content = my_func.readlines()
-print("Количество строк в файле - {}".format(len(content)))
-
-my_func = open("text2.txt", "r")
-content = my_func.read()
-content = content.split()
-print("Общее количество слов - {}".format(len(content)))
-my_func.close()
--- lesson5/exp6.py
+++
@@ -1,8 +0,0 @@
-subj = {}
-with open("text6.txt", "r") as my_file:
-    for i in my_file:
-        subject, lecture, practice, lab = i.split()
-        subj[subject] = int(lecture) + int(practice) + int(lab)
-    print("Общее количество часов по предмету - {}".format(subj))
-
-
