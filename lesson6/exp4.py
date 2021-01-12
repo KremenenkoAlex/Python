bfc4a1776c63c60231d2461594ee827582dee0af
kremenenko <kremenenko2008@mail.ru>
2021-01-12 22:49:10 +0300

    lesson6.1

--- lesson6/exp4.py
+++ lesson6/exp4.py
@@ -7,16 +7,16 @@ class Car:
         self.is_polis = is_police
 
     def go(self):
-        return 'The {} went.'.format(self.name)
+        return 'Машина {} едит.'.format(self.name)
 
     def stop(self):
-        return '\nThe {} has stopped.'.format(self.name)
+        return '\nМашина {} остановилась.'.format(self.name)
 
     def turn(self, direction):
-        return '\nThe {} turned {}'.format(self.name, direction)
+        return '\nМашина {} повернула {}'.format(self.name, direction)
 
     def show_speed(self):
-        return '\nYour speed is {}'.format(self.speed)
+        return '\nВаша скорость {}'.format(self.speed)
 
 class TownCar(Car):
     def show_speed(self):
@@ -39,13 +39,13 @@ class PoliceCar(Car):
        pass
 
 town = TownCar('BMW', 100, 'black', False)
-print('1:\n' + town.go(), town.show_speed(), town.turn('left'), town.turn('right'), town.stop())
+print('1:\n' + town.go(), town.show_speed(), town.turn('налево'), town.turn('направо'), town.stop())
 
 sport = SportCar('Formula 1', 300, 'white', False)
-print('2:\n' + sport.go(), sport.show_speed(), sport.turn('left'), sport.stop())
+print('2:\n' + sport.go(), sport.show_speed(), sport.turn('направо'), sport.stop())
 
 work = WorkCar('KAMAZ', 35, 'red', False)
-print('3:\n' + work.go(), work.show_speed(), work.turn('right'), work.stop())
+print('3:\n' + work.go(), work.show_speed(), work.turn('направо'), work.stop())
 
 police = PoliceCar('MERSEDES', 100, 'blue', True)
-print('4:\n' + work.go(), work.show_speed(), work.turn('right'), work.stop())
+print('4:\n' + work.go(), work.show_speed(), work.turn('направо'), work.stop())
